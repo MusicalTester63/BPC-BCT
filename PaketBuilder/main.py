9b77d0e0b45452a3fab7d24a3bdd9ef8a84045b8
MusicalTeser63 <TheDavid561@gmail.com>
2023-01-26 17:50:49 +0100

    test

--- PaketBuilder/main.py
+++ PaketBuilder/main.py
@@ -1,11 +1,14 @@
 from scapy.all import *
-from scapy.layers.inet6 import IPv6ExtHdrRouting, IPv6
+from scapy.layers.inet6 import IPv6, IPv6ExtHdrRouting
+from scapy.plist import PacketList
+from scapy.utils import wrpcap
 
 from paket import paket
 import pickle
 
 run = True
-dir = '/home/xhamra02/Desktop/'
+#dir = '/home/xhamra02/Desktop/'
+dir = ''
 suffix = '.cap'
 
 print("Vitajte v programe PaketBuilder v1, ktorý používa Scapy")
@@ -13,53 +16,64 @@ while run:
 
     print("1. Vytvoriť paket")
     print("2. Uložiť paket ako predvolbu")
-    print("Vypísať predvolby")
+    print("3. Vypísať predvolby")
+    print("4. Vypísať nastavený paket")
+    print("5. Exportovať paket do PCAP súboru")
     print("0. Exit")
 
-    p = None
+    control = input("")
 
-    od = input("Zadajte adresu odosielateľa: ")
-    sl = int(input("Zadajte meziľahlých uzlov: "))
-    pr = input("Zadajte adaresu príjemcu: ")
+    if(control == 1):
 
-    segList = []
-    iterator = sl
-    while iterator > 0:
-        segList.append(input("Zadajte adresu ďalšieho segmentu: "))
-        iterator -= 1
+        od = input("Zadajte adresu odosielateľa: ")
+        sl = int(input("Zadajte meziľahlých uzlov: "))
+        pr = input("Zadajte adaresu príjemcu: ")
+
+        segList = []
+        iterator = sl
+        while iterator > 0:
+            segList.append(input("Zadajte adresu ďalšieho segmentu: "))
+            iterator -= 1
+
+        meno = input("Zadajte meno paketu/paketom: ")
+        p = paket(meno, od, pr, typ, sl, segList)
+
+        typ = []
+        pktAmount = int(input("Kolko takýchto paketov si prajete vytvoriť?: "))
+        while pktAmount > 0:
+            typ.append(4)
+            pktAmount -= 1
+
+        print("meno: " + p.get_meno())
+        print("Odosielatel: " + p.get_od())
+        print("Príjemca: " + p.get_pr())
+        print("typ:", p.get_typ()[0])
+        print("Počet zostávajucich segmentov:", p.get_segmentsLeft())
+        print("Zoznam segmentov:", p.get_segmentList())
+
+        paket = IPv6(src=p.get_od(), dst=p.get_pr()) / IPv6ExtHdrRouting(type=p.get_typ(), segleft=p.get_segmentsLeft(), addresses=p.get_segmentList())
 
 
 
-    typ = []
-    pktAmount = int(input("Kolko takýchto paketov si prajete vytvoriť?: "))
-    while pktAmount > 0:
-        typ.append(4)
-        pktAmount -= 1
+    elif(control == 2):
+        print("cic")
 
-    meno = input("Zadajte meno paketu/paketom: ")
+    elif(control == 3):
+        print("cic")
 
-    p = paket(meno, od, pr, typ, sl, segList)
+    elif(control == 4):
+        paket.display()
 
-    print("meno: " + p.get_meno())
-    print("Odosielatel: " + p.get_od())
-    print("Príjemca: " + p.get_pr())
-    print("typ:", p.get_typ()[0])
-    print("Počet zostávajucich segmentov:", p.get_segmentsLeft())
-    print("Zoznam segmentov:", p.get_segmentList())
+    elif(control == 5):
 
-    paket = IPv6(src=p.get_od(), dst=p.get_pr()) / IPv6ExtHdrRouting(type=p.get_typ(), segleft=p.get_segmentsLeft(), addresses=p.get_segmentList())
+        plist = PacketList([p for p in paket])
+        [p.display() for p in plist]
 
-    plist = PacketList([p for p in paket])
-    plist.summary()
-    [p.display() for p in plist]
+        filename = input("Zadajte meno súboru: ")
 
-    filename = input("Zadajte meno súboru: ")
+        wrpcap(dir + filename + suffix, plist)
 
-    wrpcap(dir + filename + suffix, plist)
+        print("Pakety boli úspešne vygenerované a uložené do pcap súboru ", filename + suffix)
 
-    print("Pakety boli úspešne vygenerované a uložené do pcap súboru ", filename + suffix)
-
-    if input("Prajete si pokračovať?(Y/N): ") == "Y":
-        run = True
-    else:
+    elif(control == 0):
         run = False
